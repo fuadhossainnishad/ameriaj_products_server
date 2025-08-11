2ee910be59352621ff4a8c772eee494ecbfd9cf2
fuadhossainnishad <fuadhossainbk01@gmail.com>
2025-08-12 05:42:11 +0600

    resolve cors issue

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,9 +2,7 @@ import AuthRouter from "../module/auth/auth.routes";
 import HealthRouter from "../module/health/health.routes";
 import express from "express";
 import UserRouter from "../module/user/user.routes";
-import VendorRouter from "../module/vendor/vendor.routes";
 import AdminRouter from "../module/admin/admin.routes";
-import ReviewRouter from "../module/review/review.routes";
 import StripeRouter from "../module/stripe/stripe.routes";
 import NotificationRouter from "../module/notification/notification.routes";
 import SettingsRouter from "../module/settings/settings.routes";
@@ -18,12 +16,10 @@ const moduleRoutes = [
   { path: "/health", route: HealthRouter },
   { path: "/auth", route: AuthRouter },
   { path: "/user", route: UserRouter },
-  { path: "/vendor", route: VendorRouter },
   { path: "/admin", route: AdminRouter },
   { path: "/appointment", route: AppointmentRouter },
   { path: "/settings", route: SettingsRouter },
   { path: "/track", route: TrackRouter },
-  { path: "/order", route: ReviewRouter },
   { path: "/subscription", route: SubscriptionRouter },
   { path: "/payment", route: StripeRouter },
   { path: "/notification", route: NotificationRouter },
